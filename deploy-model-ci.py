24e612b161c13da04bc262f0b0d711a5b99e7fe4
Merce Chan <merce_chan@yahoo.com>
2022-04-06 10:12:23 -0700

    Create deploy-model-ci.py

---
+++ deploy-model-ci.py
@@ -0,0 +1,25 @@
+from azureml.core.model import InferenceConfig
+from azureml.core.environment import Environment, CondaDependencies
+from azureml.core.webservice import AciWebservice
+
+
+# TODO: load registered model from azure registry
+
+env = Environment.from_pip_requirements(name="onnxruntime_env", file_path='./model_requirements.txt')
+env.register(workspace=ws)
+
+inference_config = InferenceConfig(environment=env, source_directory='./source_dir', entry_script='./score_real.py')
+
+deployment_config = AciWebservice.deploy_configuration(
+    cpu_cores=1, memory_gb=1, auth_enabled=True
+)
+
+service = Model.deploy(
+    ws,
+    "myservice",
+    [model],
+    inference_config,
+    deployment_config,
+    overwrite=True,
+)
+service.wait_for_deployment(show_output=True)
